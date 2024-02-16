bdc87282fc561e0399743af17762ef7980419b84
kasif Ali <kasifali009@gmail.com>
2024-02-16 10:45:32 +0530

    testing deploy

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const userRoutes = require("./routes/UserRoutes");
 const mongoose = require("mongoose");
-require('dotenv').config();
+require("dotenv").config();
 const app = express();
-
+const ATPORT = process.env.PORT || 5000;
+const listEndpoints = require('express-list-endpoints')
 app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/netflix", {
+  .connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -22,6 +23,7 @@ mongoose
 
 app.use("/api/user", userRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log("server started on port 5000");
+app.listen(ATPORT, () => {
+  console.log("server started on port ", ATPORT );
+
 });
